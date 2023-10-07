06e2c554edead07c61166e9cb8ceb04b65766b7c
Yaroslav Liashchenko <liaroswin@gmail.com>
2023-10-07 14:21:52 +0300

    replaced BookRepositoryImpl by using JpaRepository

--- src/main/java/book/store/exception/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-package book.store.exception;
-
-public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
