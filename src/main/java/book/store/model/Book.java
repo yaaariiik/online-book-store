4978bd8a243646cb8c76e2ad9b01e759036e100d
Yaroslav Liashchenko <liaroswin@gmail.com>
2023-10-10 18:59:49 +0300

    implemented soft delete concept for the Book entity

--- src/main/java/book/store/model/Book.java
+++ src/main/java/book/store/model/Book.java
@@ -8,22 +8,36 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String author;
+
     @Column(unique = true, nullable = false)
     private String isbn;
+
     @Column(nullable = false)
     private BigDecimal price;
+
     private String description;
+
+    @Column(name = "cover_image")
     private String coverImage;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 }
